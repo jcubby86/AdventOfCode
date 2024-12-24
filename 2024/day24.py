95d33e5c141e563f77ad35de4a3b6fc1ea3917a5
Jacob Bastian <jcubby86@hotmail.com>
2024-12-24 12:58:16 -0700

    day 24 part 2 (sort of)

--- 2024/day24.py
+++ 2024/day24.py
@@ -1,43 +1,105 @@
 #!/usr/bin/env python3
 
+from functools import cache
 import sys
-import re
 from collections import deque
 
-def readData(fileName: str) -> tuple:
+
+def readData(fileName: str) -> tuple[list, list]:
     with open(fileName, "r") as f:
-        values, rules = f.read().split("\n\n")
-        
-        return [tuple(x.split(": ")) for x in values.split("\n")], deque([x.split() for x in rules.split("\n")])
+        v, r = f.read().split("\n\n")
+        values = [x.split(": ") for x in v.split("\n")]
+        rules = [x.split() for x in r.split("\n")]
+        return values, rules
+
     return []
 
 
-def part1(values: list[tuple], rules: deque[tuple]) -> int:
-    lookup = {}
-
-    for k, v in values:
-        lookup[k] = int(v)
-
+def executeRules(values, rules):
     while len(rules) > 0:
         r = rules.popleft()
-        if r[0] not in lookup or r[2] not in lookup:
+        if r[0] not in values or r[2] not in values:
             rules.append(r)
             continue
 
-        if r[1] == 'AND':
-            lookup[r[4]] = lookup[r[0]] & lookup[r[2]]
-        elif r[1] == 'OR':
-            lookup[r[4]] = lookup[r[0]] | lookup[r[2]]
-        elif r[1] == 'XOR':
-            lookup[r[4]] = lookup[r[0]] ^ lookup[r[2]]
-
-    f = sorted([(k, v) for k, v in lookup.items() if k[0] == 'z'], key=lambda x: int(x[0][1:]))
-
-    return int(''.join([str(x[1]) for x in f[::-1]]), 2)
+        if r[1] == "AND":
+            values[r[4]] = values[r[0]] & values[r[2]]
+        elif r[1] == "OR":
+            values[r[4]] = values[r[0]] | values[r[2]]
+        elif r[1] == "XOR":
+            values[r[4]] = values[r[0]] ^ values[r[2]]
 
 
-def part2(data):
-    pass
+def getIntValue(values: dict, prefix: str) -> int:
+    f = sorted(
+        [(k, v) for k, v in values.items() if k[0] == prefix],
+        key=lambda x: int(x[0][1:]),
+    )
+    return int("".join([str(x[1]) for x in f[::-1]]), 2)
+
+
+def getBitValues(value: int) -> str:
+    return f"{value:050b}"
+
+
+def part1(initialValues: list, rules: list) -> int:
+    values = {k: int(v) for k, v in initialValues} 
+    executeRules(values, deque(rules))
+    return getIntValue(values, "z")
+
+
+def part2(initialValues: list, rules: list):
+    ruleDict = {x[4]: x for x in rules}
+    values = {k: int(v) for k, v in initialValues} 
+
+    @cache
+    def getDependencies(key: str) -> set[str]:
+        d: set[str] = set()
+        if key not in ruleDict:
+            return d
+
+        rule = ruleDict[key]
+        d.add(rule[0])
+        d.update(getDependencies(rule[0]))
+        d.add(rule[2])
+        d.update(getDependencies(rule[2]))
+
+        return d
+    
+    def executeRule(key: str):
+        r = ruleDict[key]
+
+        if r[0] not in values:
+            executeRule(r[0])
+        if r[2] not in values:
+            executeRule(r[2])
+        if r[1] == "AND":
+            values[r[4]] = values[r[0]] & values[r[2]]
+        elif r[1] == "OR":
+            values[r[4]] = values[r[0]] | values[r[2]]
+        elif r[1] == "XOR":
+            values[r[4]] = values[r[0]] ^ values[r[2]]
+    
+
+
+    x = getIntValue(values, "x")
+    y = getIntValue(values, "y")
+    expected = getBitValues(x + y)
+
+    for i in range(50):
+        key = f"z{i:02d}"
+        e = int(expected[-i - 1])
+        if key not in ruleDict:
+            break
+        executeRule(key)
+        if e != values[key]:
+            print(f"key: {key}, expected: {e}, actual: {values[key]}")
+            print(getDependencies(key).difference(getDependencies(f"z{(i-1):02d}")))
+    # from here it's a manual process, but this helps narrow down the wires that could be crossed.
+    # make sure that z00 is the XOR of (x00 XOR y00) and the wire that's "carrying" the 1 from before
+
+
+    return expected
 
 
 if __name__ == "__main__":
@@ -45,7 +107,7 @@ if __name__ == "__main__":
         print("missing file name")
         sys.exit(1)
 
-    data = readData(sys.argv[1])
+    values, rules = readData(sys.argv[1])
 
-    print("part 1:", part1(*data))
-    print("part 2:", part2(data))
+    print("part 1:", part1(values, rules))
+    print("part 2:", part2(values, rules))
